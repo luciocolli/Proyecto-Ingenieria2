4e5a9e06e68267572d4b96c860cf0416ba14a579
Lucio Colli <luciocolli1@gmail.com>
2024-05-02 11:46:38 -0300

    index.html, primer view, primer urls de users e include en hopeTrade de urls de users

--- hopeTrade/users/views.py
+++ hopeTrade/users/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+#from .models import User ---> para un futuro   
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
