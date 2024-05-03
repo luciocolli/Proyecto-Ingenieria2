82179fe85347922fb8078c9086ca3b55cb730dda
Julian-Garcia <julian020404@gmail.com>
2024-05-03 12:55:09 -0300

    Login

--- hopeTrade/users/views.py
+++ hopeTrade/users/views.py
@@ -1,16 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import User
+from .forms import CreateNewUser, CreatelogIn
+from django.contrib.auth import login, authenticate
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
-def login(request):
-    return render(request, 'login.html')
 
-from .forms import CreateNewUser
+
+def login_view(request):
+
+    if request.method == 'GET':
+        return render(request, 'login.html', {
+            'form' : CreatelogIn()
+        })
+    else:
+        if request.method == 'POST':
+            dni = request.POST['dni']
+            password = request.POST['password']
+            user = authenticate(request,dni = dni , password = password)
+            if user is not None:
+                login(request, user)
+                return redirect('holanda')
+            else:
+                return redirect('register')
+        else:
+            return render(request, 'login.html')
+
+
 
 def register(request):
     
@@ -30,3 +51,12 @@ def register(request):
             password = request.POST['password']
         )
         return redirect('login')
+
+def authenticate(request, dni=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        try:
+            usuario = User.objects.get(dni=dni)
+            if usuario.password == password:
+                return usuario
+        except User.DoesNotExist:
+            return None
